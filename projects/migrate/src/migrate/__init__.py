d12aa88037840db20a21b7be02af32a120205490
Jim Lundin <jimeriklundin@gmail.com>
2025-08-24 22:29:33 +0200

    fix(migrate): improper module export

--- projects/migrate/src/migrate/__init__.py
+++ projects/migrate/src/migrate/__init__.py
@@ -1,5 +1,6 @@
 """Convert module for DPM Toolkit."""
 
-from migrate.main import create_access_engine, migrate_to_sqlite
+from migrate.main import migrate_to_sqlite
+from migrate.processing import create_access_engine
 
 __all__ = ["create_access_engine", "migrate_to_sqlite"]
