293c8c066088009cfb618d12d77066138b2ff4de
Jim Lundin <jimeriklundin@gmail.com>
2025-08-24 15:13:46 +0200

    refactor(migrate): extract file handling to cli

--- projects/migrate/src/migrate/main.py
+++ projects/migrate/src/migrate/main.py
@@ -1,58 +1,26 @@
 """Main module for database conversion."""
 
-from datetime import UTC, datetime
-from logging import getLogger
-from pathlib import Path
+from sqlite3 import Connection
 
-from sqlalchemy import create_engine, text
+from sqlalchemy import Engine, create_engine
 
-from migrate.processing import (
-    create_access_engine,
-    extract_schema_and_data,
-    load_data,
-)
-
-logger = getLogger(__name__)
+from migrate.processing import schema_and_data, load_data
 
 
-def migrate_to_sqlite(source: Path, target: Path) -> None:
+def migrate_to_sqlite(source: Engine) -> Connection:
     """Migrate Access database to SQLite.
 
     Args:
         source: Directory or path to Access database
-        target: Directory to store SQLite database
+
+    Returns:
+        Connection to the new SQLite database
 
     """
-    start_time = datetime.now(UTC)
+    metadata, tables = schema_and_data(source)
 
-    if not source.exists():
-        logger.error("Source %s does not exist", source)
-        return
-    if source.suffix not in (".mdb", ".accdb"):
-        logger.error("Source must be an Access database file (.mdb or .accdb)")
-        return
-
-    if target.exists():
-        logger.error("Target database already exists.")
-        return
-    if target.suffix not in (".sqlite", ".db"):
-        logger.error("Target must be a SQLite database file (.sqlite or .db)")
-        return
-
-    logger.info("Processing: %s", source.stem)
-
-    access = create_access_engine(source)
-    metadata, tables = extract_schema_and_data(access)
-
-    target.parent.mkdir(parents=True, exist_ok=True)
-
-    sqlite = create_engine("sqlite:///:memory:")
+    sqlite = create_engine("sqlite://")
     metadata.create_all(sqlite)
     load_data(sqlite, tables)
 
-    with sqlite.connect() as connection:
-        connection.execute(text(f"VACUUM INTO '{target}'"))
-        logger.info("Saved: %s", target)
-
-    stop_time = datetime.now(UTC)
-    logger.info("Migrated database in %s", stop_time - start_time)
+    return sqlite.raw_connection().connection
