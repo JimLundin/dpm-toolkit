5abf7fde5bbeccabc93eb8414c8cce76628e1e3e
Jim Lundin <jimeriklundin@gmail.com>
2025-08-25 10:56:01 +0200

    fix(cli): final touchups

--- projects/compare/src/compare/__init__.py
+++ projects/compare/src/compare/__init__.py
@@ -1,13 +1,9 @@
 """Database comparison module for SQLite databases."""
 
-from compare.main import (
-    compare_dbs,
-    comparisons_to_html,
-    comparisons_to_json,
-)
+from compare.main import compare_databases, comparisons_to_html, comparisons_to_json
 
 __all__ = [
-    "compare_dbs",
+    "compare_databases",
     "comparisons_to_html",
     "comparisons_to_json",
 ]
