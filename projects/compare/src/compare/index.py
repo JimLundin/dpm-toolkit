ebd233c9ff7906c28e65f8ed8a6cd0456aa9fa8b
Jim Lundin <jimeriklundin@gmail.com>
2025-09-03 15:06:27 +0200

    refactor(compare): fix index comparison logic to correctly match the priority

--- projects/compare/src/compare/index.py
+++ projects/compare/src/compare/index.py
@@ -4,7 +4,8 @@ from collections.abc import Callable, Iterator
 from sqlite3 import Row
 
 type ValueType = str | int | float | None
-type IndexKey = tuple[ValueType, ...]
+type Key = tuple[ValueType, ...]
+type IndexKey = tuple[str, Key]
 type IndexKeys = list[IndexKey]
 type Indexer = Callable[[Row], IndexKeys]
 
@@ -15,13 +16,13 @@ class HierarchicalIndex:
     def __init__(self, indexer: Indexer) -> None:
         """Initialize the index with an indexer function."""
         self._indexer = indexer
-        self._key_to_group_id: dict[tuple[int, IndexKey], int] = {}
-        self._groups: dict[int, tuple[Row, tuple[tuple[int, IndexKey], ...]]] = {}
+        self._key_to_group_id: dict[IndexKey, int] = {}
+        self._groups: dict[int, tuple[Row, tuple[IndexKey, ...]]] = {}
         self._next_group_id = 0
 
     def add(self, row: Row) -> None:
-        """Add a row with automatically generated hierarchical keys."""
-        keys = tuple(enumerate(self._indexer(row)))
+        """Add a row with semantic keys."""
+        keys = tuple(self._indexer(row))
 
         group_id = self._next_group_id
         self._next_group_id += 1
@@ -32,16 +33,15 @@ class HierarchicalIndex:
             self._key_to_group_id[key] = group_id
 
     def pop(self, row: Row) -> Row | None:
-        """Pop first available match for the given row, trying keys by priority."""
-        for priority, key in enumerate(self._indexer(row)):
-            if matched_row := self._pop_by_key(priority, key):
+        """Pop first available match for the given row, trying keys in order."""
+        for key in self._indexer(row):
+            if matched_row := self._pop_by_key(key):
                 return matched_row
         return None
 
-    def _pop_by_key(self, priority: int, key: IndexKey) -> Row | None:
-        """Pop value by specific priority and key, removing from all keys."""
-        lookup_key = (priority, key)
-        group_id = self._key_to_group_id.get(lookup_key)
+    def _pop_by_key(self, key: IndexKey) -> Row | None:
+        """Pop value by specific key, removing from all keys."""
+        group_id = self._key_to_group_id.get(key)
         if group_id is None:
             return None
 
