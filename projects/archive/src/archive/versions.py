0d343b9b0ed97ff700f30ec234fde402683d33f7
Jim Lundin <jimeriklundin@gmail.com>
2025-09-17 15:08:04 +0200

    refactor(cli): change to use cyclopts instead of typer

--- projects/archive/src/archive/versions.py
+++ projects/archive/src/archive/versions.py
@@ -3,38 +3,18 @@
 from collections import defaultdict
 from collections.abc import Iterable
 from datetime import date
-from enum import StrEnum, auto
 from pathlib import Path
 from tomllib import load
-from typing import NotRequired, TypedDict
+from typing import Literal, NotRequired, TypedDict
 
 type VersionUrls = dict[str, set[str]]
 
 
-class VersionType(StrEnum):
-    """Enumeration for version types."""
+type VersionType = Literal["sample", "draft", "final", "release", "errata"]
 
-    SAMPLE = auto()
-    DRAFT = auto()
-    FINAL = auto()
-    RELEASE = auto()
-    ERRATA = auto()
+type Group = Literal["all", "release", "draft"]
 
-
-class Group(StrEnum):
-    """Enumeration for version groups."""
-
-    ALL = auto()
-    RELEASE = auto()
-    DRAFT = auto()
-
-
-class SourceType(StrEnum):
-    """Enumeration for source types."""
-
-    ORIGINAL = auto()
-    ARCHIVE = auto()
-    CONVERTED = auto()
+type SourceType = Literal["original", "archive", "converted"]
 
 
 class Source(TypedDict):
@@ -61,11 +41,11 @@ class Version(TypedDict):
 
 def get_source(version: Version, source_type: SourceType) -> Source:
     """Get source by type from version."""
-    if source_type == SourceType.ORIGINAL:
+    if source_type == "original":
         return version["original"]
-    if source_type == SourceType.ARCHIVE:
+    if source_type == "archive":
         return version["archive"]
-    if source_type == SourceType.CONVERTED:
+    if source_type == "converted":
         return version["converted"]
     return None
 
